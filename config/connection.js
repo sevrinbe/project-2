2cad56b69646a1630061dcdd9eee7dcb1294efc3
gwhitney15 <106910126+gwhitney15@users.noreply.github.com>
2022-09-26 18:47:11 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.DB_N);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
